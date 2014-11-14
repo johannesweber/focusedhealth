fe2260e085cdb5f1252973ab03ec3aaba41db2f9
Johannes Weber <weber.johanes@gmail.com>
2014-11-14 14:34:26 +0100

    added withings receive.php

--- fitbit/receive/index.php
+++ fitbit/receive/index.php
@@ -2,9 +2,9 @@
 
 require '../api/fitbitphp.php';
 
-$token =  $_POST['oauth_token'];
+$oauth_token =  $_POST['oauth_token'];
 
-$secret = $_POST['oauth_token_secret'];
+$oauth_token_secret = $_POST['oauth_token_secret'];
 
 $fitbit = new FitBitPHP("7c39abf127964bc984aba4020845ff11", "18c4a92f21f1458e8ac9798567d3d38c");
 $fitbit->setOAuthDetails($token, $secret);
---
+++ withings/receive/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$oauth_token =  $_POST['oauth_token'];
+
+$oauth_token_secret = $_POST['oauth_token_secret'];
+
+$user_id = $_POST['user_id'];
+
+?>
