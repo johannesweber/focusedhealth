45b5321ce563641d2225f5cd737de6a8d2f6837e
sloudown <timon.vogler@arcor.de>
2014-12-01 17:38:32 +0100

    verbessert: keine doppelten daten beim erneuten synchronisieren

--- fitbit/weight/insert/insert_weight.php
+++ fitbit/weight/insert/insert_weight.php
@@ -7,7 +7,6 @@
  */
 
 
-
 $timestamp = time();
 $datum = date("Y-m-d", $timestamp);
 
@@ -22,22 +21,46 @@ print_r($response);
 $arrayLenght = $response->weight;
 $arrayLenght = sizeof($arrayLenght);
 
+
 for ($x = 0; $x < $arrayLenght; $x++) {
 
     $weight = $response->weight[$x]->weight;
     $bmi = $response->weight[$x]->bmi;
     $date = $response->weight[$x]->date;
 
+
+    //SQL Statement to
+    $select_weight = "SELECT * FROM value WHERE user_id='42' AND measurement_id='$weightId' AND company_id='$company_id' AND date= '$date' ";
+    $result = $db_connection->executeStatement($select_weight);
+    $rowCount = $result->num_rows;
+
+//weight was not inserted today
+    if ($rowCount == 0) {
+
+
 //SQL Statement to insert data into value table
-$insert_weight = "INSERT INTO value (user_id, measurement_id, company_id, value, date)
+        $insert_weight = "INSERT INTO value (user_id, measurement_id, company_id, value, date)
         VALUES ('42', '$weightId', '$company_id', '$weight','$date'),
                 ('42', '$bmiId', '$company_id', '$bmi','$date')";
 
-$db_connection->executeStatement($insert_weight);
+        $db_connection->executeStatement($insert_weight);
 
+
+    } else {
+
+        $update_weight = "UPDATE value set value = '$weight'
+                                     WHERE user_id='42' AND measurement_id='$weightId' AND company_id='$company_id' AND date = '$datum'";
+
+        $db_connection->executeStatement($update_weight);
+
+
+        $update_weight = "UPDATE value set value = '$bmi'
+                                     WHERE user_id='42' AND measurement_id='$bmiId' AND company_id='$company_id' AND date = '$datum'";
+
+        $db_connection->executeStatement($update_weight);
+
+    }
 
 }
-
-
 
 ?>
