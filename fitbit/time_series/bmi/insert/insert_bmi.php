6e25c51a43936d3139b65cc5d69379609ce3e3e7
Patric Auer <patric.auer@googlemail.com>
2014-12-03 12:08:19 +0100

    bmi hinzugefügt in time series

---
+++ fitbit/time_series/bmi/insert/insert_bmi.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: timonvogler
+ * Date: 02.12.14
+ * Time: 15:12
+ */
+
+
+
+
+include '../../id/find_company_id.php';
+include '../../id/find_bmi_id.php';
+
+
+
+$response = $fitbit->getTimeSeries("bmi","today","7d");
+print_r($response);
+
+
+
+
+echo $arrayLenght = $response;
+echo $arrayLenght = sizeof($arrayLenght);
+
+
+$array = $response;
+
+for ($x = 0; $x < $arrayLenght; $x++) {
+
+    $bmi = $array[$x]->value;
+    $date = $array[$x]->dateTime;
+
+
+
+
+    //SQL Statement to
+    $select_bmi = "SELECT * FROM value WHERE user_id='42' AND measurement_id='$bmiId' AND company_id='$company_id' AND date= '$date' ";
+    $result = $db_connection->executeStatement($select_bmi);
+    $rowCount = $result->num_rows;
+
+//weight was not inserted today
+    if ($rowCount == 0) {
+
+
+//SQL Statement to insert data into value table
+        $insert_bmi = "INSERT INTO value (user_id, measurement_id, company_id, value, date)
+        VALUES ('42', '$bmiId', '$company_id', '$bmi','$date')";
+
+        $db_connection->executeStatement($insert_bmi);
+
+
+    } /* else {
+
+        $update_weight = "UPDATE value set value = '$weight'
+                                     WHERE user_id='42' AND measurement_id='$weightId' AND company_id='$company_id' AND date = '$datum'";
+
+        $db_connection->executeStatement($update_weight);
+
+
+    } */
+
+}
+
+?>
---
+++ fitbit/time_series/bmi/select/index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: timonvogler
+ * Date: 02.12.14
+ * Time: 18:00
+ */
+
+
+include '../../../../db_connection.php';
+
+
+$db_connection = new DatabaseConnection();
+
+$db_connection->connect();
+
+
+include 'find_bmi_id.php';
+
+
+$fetch_food = "SELECT value, date FROM value WHERE user_id='42' AND measurement_id = '$bmiId'";
+
+$db_connection->executeStatement($fetch_food);
+$result = $db_connection->getResultAsJSON();
+
+
+print_r($result);
