93578c9926a2816c55531fae9ecfd2e898510f79
sloudown <timon.vogler@arcor.de>
2014-12-04 14:54:14 +0100

    bodymeasurements angepasst

--- fitbit/body_measurements/insert/insert_body_measurements.php
+++ fitbit/body_measurements/insert/insert_body_measurements.php
@@ -13,34 +13,34 @@ $response = $fitbit->getBody($date);
 print_r($response);
 
 $bicep = $response->body->bicep;
-$bmi = $response->body->bmi;
+//$bmi = $response->body->bmi;
 $calf = $response->body->calf;
 $chest = $response->body->chest;
-$fat = $response->body->fat;
+//$fat = $response->body->fat;
 $forearm = $response->body->forearm;
 $hips = $response->body->hips;
 $neck = $response->body->neck;
 $thigh = $response->body->thigh;
 $waist = $response->body->waist;
-$weight = $response->body->weight;
+//$weight = $response->body->weight;
 
 include '../id/find_bicep_id.php';
-include '../id/find_bmi_id.php';
+//include '../id/find_bmi_id.php';
 include '../id/find_calf_id.php';
 include '../id/find_chest_id.php';
-include '../id/find_body_fat_id.php';
+//include '../id/find_body_fat_id.php';
 include '../id/find_forearm_id.php';
 include '../id/find_hips_id.php';
 include '../id/find_neck_id.php';
 include '../id/find_thigh_id.php';
 include '../id/find_waist_id.php';
-include '../id/find_weight_id.php';
+//include '../id/find_weight_id.php';
 
 
 
-$measurementIdArray = array($bicepId, $bmiId, $calfId, $chestId, $fatId, $forearmId, $hipsId, $neckId, $thighId, $waistId, $weightId);
+$measurementIdArray = array($bicepId, $calfId, $chestId, $forearmId, $hipsId, $neckId, $thighId, $waistId);
 
-$measurementArray = array($bicep, $bmi, $calf, $chest, $fat, $forearm, $hips, $neck, $thigh, $waist, $weight);
+$measurementArray = array($bicep, $calf, $chest, $forearm, $hips, $neck, $thigh, $waist);
 
 
 
@@ -62,18 +62,7 @@ if ($rowCount == 0) {
     $insert_body = "INSERT INTO value (user_id, measurement_id, company_id, value, date)
          VALUES ('42', '$ID', '$company_id', '$value','$date')";
 
-            /*
-                ('42', '$bmiId', '$company_id', '$bmi','$date'),
-                ('42', '$calfId', '$company_id', '$calf','$date'),
-                ('42', '$chestId', '$company_id', '$chest','$date'),
-                ('42', '$fatId', '$company_id', '$fat','$date'),
-                ('42', '$forearmId', '$company_id', '$forearm','$date'),
-                ('42', '$hipsId', '$company_id', '$hips','$date'),
-                ('42', '$neckId', '$company_id', '$neck','$date'),
-                ('42', '$thighId', '$company_id', '$thigh','$date'),
-                ('42', '$waistId', '$company_id', '$waist','$date'),
-                ('42', '$weightId', '$company_id', '$weight','$date')";
-*/
+        
     $db_connection->executeStatement($insert_body);
 
 
