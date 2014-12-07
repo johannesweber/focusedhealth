e4a97cd65ece5b7cf30a6bdee57d74399be7dc6f
Johannes Weber <weber.johanes@gmail.com>
2014-12-07 19:55:08 +0100

    fixed .../time_series/water

---
+++ fitbit/time_series/water/insert_water.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pauer
+ * Date: 03.12.14
+ * Time: 13:52
+ */
+
+
+include '../../id/find_company_id.php';
+include '../../id/find_water_id.php';
+
+$response = $fitbit->getTimeSeries("water", "today", "7d");
+
+$arrayLenght = $response;
+$arrayLenght = sizeof($arrayLenght);
+
+
+$array = $response;
+
+for ($x = 0; $x < $arrayLenght; $x++) {
+
+    $water = $array[$x]->value;
+    $date = $array[$x]->dateTime;
+
+
+    //SQL Statement to
+    $select_water = "SELECT * FROM value WHERE user_id='42' AND measurement_id='$waterId' AND company_id='$company_id' AND date= '$date' ";
+    $result = $db_connection->executeStatement($select_water);
+    $rowCount = $result->num_rows;
+
+//weight was not inserted today
+    if ($rowCount == 0) {
+
+
+//SQL Statement to insert data into value table
+        $insert_water = "INSERT INTO value (user_id, measurement_id, company_id, value, date)
+        VALUES ('42', '$waterId', '$company_id', '$water','$date')";
+
+        $db_connection->executeStatement($insert_water);
+
+
+    }  else {
+
+
+        $update = "UPDATE value SET value = '$water'
+                                     WHERE user_id='42' AND measurement_id='$waterId' AND company_id='$company_id' AND date = '$date'";
+
+        $db_connection->executeStatement($update);
+
+
+    }
+
+}
+
+?>
---
+++ fitbit/time_series/water/select_water.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pauer
+ * Date: 03.12.14
+ * Time: 13:52
+ */
+
+$userId = '42';
+
+include '../../id/find_water_id.php';
+
+
+$fetch = "SELECT value, date, waterUnit
+        FROM value
+        JOIN company_account_info AS cai on value.user_id = cai.user_id
+        WHERE value.user_id='$userId' AND value.measurement_id = '$waterId'";
+
+$db_connection->executeStatement($fetch);
+$result = $db_connection->getResultAsJSON();
+
+echo $result;
+
+?>
