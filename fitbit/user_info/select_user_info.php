3a83b5e975b08f38fa8e77d8ec71872b2d24e0b5
Johannes Weber <weber.johanes@gmail.com>
2014-12-09 14:44:06 +0100

    added select_user_info

--- fitbit/user_info/select_user_info.php
+++ fitbit/user_info/select_user_info.php
@@ -6,12 +6,10 @@
  * Time: 13:39
  */
 
-
-
 include '../id/find_company_account_id.php';
 
 //TODO real User id required
-$fetch_user_info = "SELECT city,country, dateOfBirth, gender, height, memeberSince, timzone FROM company_account_info WHERE company_account_id='$companyAccountId'";
+$fetch_user_info = "SELECT city, country, dateOfBirth, gender, height, memeberSince, timzone FROM company_account_info WHERE company_account_id='$companyAccountId'";
 
 $db_connection->executeStatement($fetch_user_info);
 
