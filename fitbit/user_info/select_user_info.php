76764d14a60839285407029815ad774d9b8e80cb
sloudown <timon.vogler@arcor.de>
2014-12-09 10:47:12 +0100

    user info select angepasst

--- fitbit/user_info/select_user_info.php
+++ fitbit/user_info/select_user_info.php
@@ -11,7 +11,7 @@
 include '../id/find_company_account_id.php';
 
 //TODO real User id required
-$fetch_user_info = "SELECT * FROM company_account_info WHERE company_account_id='$companyAccountId'";
+$fetch_user_info = "SELECT city,country, dateOfBirth, gender, height, memeberSince, timzone FROM company_account_info WHERE company_account_id='$companyAccountId'";
 
 $db_connection->executeStatement($fetch_user_info);
 
