453ac94e19cdaa628edb19464381ef6c14fd6eee
sloudown <timon.vogler@arcor.de>
2014-12-10 15:17:02 +0100

    oh man

--- fitbit/authorize/index.php
+++ fitbit/authorize/index.php
@@ -23,8 +23,8 @@ $db_connection->connect();
 
 $userId = $_GET['userId'];
 
-$oauth_token = $_POST['oauth_token'];
-$oauth_token_secret = $_POST['oauth_token_secret'];
+$oauth_token = $_GET['oauth_token'];
+$oauth_token_secret = $_GET['oauth_token_secret'];
 
 $fitbit = new FitBitPHP("7c39abf127964bc984aba4020845ff11", "18c4a92f21f1458e8ac9798567d3d38c");
 $fitbit->setOAuthDetails($oauth_token, $oauth_token_secret);
