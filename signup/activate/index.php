ef691cd1b377d6e9864dbc68a22d1795954c30dc
Johannes Weber <weber.johanes@gmail.com>
2015-01-09 17:37:39 +0100

    added method for selecting duplicate measurement from a user

--- signup/activate/index.php
+++ signup/activate/index.php
@@ -20,17 +20,31 @@ if($_REQUEST['email'] && $_REQUEST['verifier']) {
 
     $dbConnection->connect();
 
-    $select_user = "SELECT email FROM user WHERE email = '$email' AND verifier = '$verifier'";
+    $select_user = "SELECT * FROM user WHERE email = '$email' AND verifier = '$verifier'";
 
     $dbConnection->executeStatement($select_user);
 
     $result = $dbConnection->getResult();
+
+    $resultArray = $dbConnection->getResultAsArray();
+
+    $userId = $resultArray["id"];
+    $company = 'focused health';
+    $companyId = $dbConnection->getCompanyId($company);
+    $company_account_id = $userId;
+    $timestamp = time();
+    $memberSince = date("Y-m-d", $timestamp);
 
     if (mysqli_num_rows($result) > 0) {
 
         $set_user_active = "UPDATE user SET active = 'Yes' WHERE email = '$email'";
 
         $dbConnection->executeStatement($set_user_active);
+
+        $addFocusedHealthCompanyToUser = "INSERT INTO user_company_account (user_id, company_id, company_account_id, timestamp)
+                                          VALUES ($userId, $companyId, $company_account_id, '$memberSince')";
+
+        $dbConnection->executeStatement($addFocusedHealthCompanyToUser);
 
         echo "Congratulations!. You have succesfully activated your Focused Health Account.";
     }
